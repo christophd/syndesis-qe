a9c497d9a86afa0954e5ae78ab1d1ee329a41a3c
Christoph Deppisch <cdeppisch@redhat.com>
2019-04-03 11:32:19 +0200

    Share configuration in Google sheets tests

--- integration-tests/src/test/java/io/syndesis/qe/itest/sheets/DBToSheets_IT.java
+++ integration-tests/src/test/java/io/syndesis/qe/itest/sheets/DBToSheets_IT.java
@@ -1,42 +1,23 @@
 package io.syndesis.qe.itest.sheets;
 
-import javax.servlet.Filter;
 import javax.sql.DataSource;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.dsl.endpoint.CitrusEndpoints;
 import com.consol.citrus.dsl.runner.TestRunner;
-import com.consol.citrus.dsl.runner.TestRunnerBeforeTestSupport;
 import com.consol.citrus.http.server.HttpServer;
-import com.consol.citrus.http.servlet.RequestCachingServletFilter;
-import io.syndesis.qe.itest.SyndesisIntegrationTestSupport;
 import io.syndesis.qe.itest.containers.integration.SyndesisIntegrationRuntimeContainer;
-import io.syndesis.qe.itest.gzip.GzipServletFilter;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.util.SocketUtils;
-import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.GenericContainer;
 
 /**
  * @author Christoph Deppisch
  */
-@ContextConfiguration(classes = DBToSheets_IT.EndpointConfig.class)
-public class DBToSheets_IT extends SyndesisIntegrationTestSupport {
-
-    private static int googleSheetsServerPort = SocketUtils.findAvailableTcpPort();
-    static {
-        Testcontainers.exposeHostPorts(googleSheetsServerPort);
-    }
+public class DBToSheets_IT extends SheetsIntegrationTestSupport {
 
     @Autowired
     private DataSource sampleDb;
@@ -73,36 +54,4 @@ public class DBToSheets_IT extends SyndesisIntegrationTestSupport {
                         .send()
                         .response(HttpStatus.OK));
     }
-
-    @Configuration
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    public static class EndpointConfig {
-
-        @Bean
-        public HttpServer googleSheetsApiServer() {
-            Map<String, Filter> filterMap = new LinkedHashMap<>();
-            filterMap.put("request-caching-filter", new RequestCachingServletFilter());
-            filterMap.put("gzip-filter", new GzipServletFilter());
-
-            return CitrusEndpoints.http()
-                    .server()
-                    .port(googleSheetsServerPort)
-                    .autoStart(true)
-                    .timeout(60000L)
-                    .filters(filterMap)
-                    .build();
-        }
-
-        @Bean
-        public TestRunnerBeforeTestSupport beforeTest(DataSource sampleDb) {
-            return new TestRunnerBeforeTestSupport() {
-                @Override
-                public void beforeTest(TestRunner runner) {
-                    runner.sql(builder -> builder.dataSource(sampleDb)
-                            .statement("delete from contact where lead_source='google-sheets'"));
-                }
-            };
-        }
-    }
-
 }
---
+++ integration-tests/src/test/java/io/syndesis/qe/itest/sheets/SheetsIntegrationTestSupport.java
@@ -0,0 +1,62 @@
+package io.syndesis.qe.itest.sheets;
+
+import javax.servlet.Filter;
+import javax.sql.DataSource;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.consol.citrus.dsl.endpoint.CitrusEndpoints;
+import com.consol.citrus.dsl.runner.TestRunner;
+import com.consol.citrus.dsl.runner.TestRunnerBeforeTestSupport;
+import com.consol.citrus.http.server.HttpServer;
+import com.consol.citrus.http.servlet.RequestCachingServletFilter;
+import io.syndesis.qe.itest.SyndesisIntegrationTestSupport;
+import io.syndesis.qe.itest.gzip.GzipServletFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.SocketUtils;
+import org.testcontainers.Testcontainers;
+
+/**
+ * @author Christoph Deppisch
+ */
+@ContextConfiguration(classes = SheetsIntegrationTestSupport.EndpointConfig.class)
+public class SheetsIntegrationTestSupport extends SyndesisIntegrationTestSupport {
+
+    static int googleSheetsServerPort = SocketUtils.findAvailableTcpPort();
+    static {
+        Testcontainers.exposeHostPorts(googleSheetsServerPort);
+    }
+
+    @Configuration
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public static class EndpointConfig {
+
+        @Bean
+        public HttpServer googleSheetsApiServer() {
+            Map<String, Filter> filterMap = new LinkedHashMap<>();
+            filterMap.put("request-caching-filter", new RequestCachingServletFilter());
+            filterMap.put("gzip-filter", new GzipServletFilter());
+
+            return CitrusEndpoints.http()
+                    .server()
+                    .port(googleSheetsServerPort)
+                    .autoStart(true)
+                    .timeout(60000L)
+                    .filters(filterMap)
+                    .build();
+        }
+
+        @Bean
+        public TestRunnerBeforeTestSupport beforeTest(DataSource sampleDb) {
+            return new TestRunnerBeforeTestSupport() {
+                @Override
+                public void beforeTest(TestRunner runner) {
+                    runner.sql(builder -> builder.dataSource(sampleDb)
+                            .statement("delete from contact where lead_source='google-sheets'"));
+                }
+            };
+        }
+    }
+}
